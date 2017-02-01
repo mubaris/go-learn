d58379b7b081e0d93c6dc4a4a93fd0984faf6772
Mubaris Hassan <mubarishassannk@gmail.com>
2017-02-02 00:47:47 +0530

    Modify Program 12 Add Maps Techniques

--- prog-12-maps.go
+++ prog-12-maps.go
@@ -11,7 +11,27 @@ func main() {
 	elements["B"] = "Boron"
 	elements["Ne"] = "Neon"
 
+	flashFamily := map[string]string{
+		"Flash":       "Barry Allen",
+		"Kid Flash":   "Wally West",
+		"Impulse":     "Bart Allen",
+		"First Flash": "Jay Garrick",
+	}
+
+	elementsDetails := map[string]map[string]string{
+		"H": map[string]string{
+			"name":  "Hydrogen",
+			"state": "Gas",
+		},
+		"Li": map[string]string{
+			"name":  "Lithium",
+			"state": "Solid",
+		},
+	}
+
 	fmt.Println(elements["He"])
+	fmt.Println(flashFamily["Impulse"])
+	fmt.Println(elementsDetails)
 
 	delete(elements, "Ne")
 }
