5d67640c3a810f7be5a41b682679eb3664ae43d7
Mubaris Hassan <mubarishassannk@gmail.com>
2017-02-01 23:55:30 +0530

    Add Programs 12 Maps

---
+++ prog-12-maps.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	elements := make(map[string]string)
+	elements["H"] = "Hydrogen"
+	elements["He"] = "Helium"
+	elements["Li"] = "Lithium"
+	elements["Be"] = "Beryllium"
+	elements["B"] = "Boron"
+	elements["Ne"] = "Neon"
+
+	fmt.Println(elements["He"])
+
+	delete(elements, "Ne")
+}
